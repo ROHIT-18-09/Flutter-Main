32b72c34f8a9ffd0cbd54506b4860b6e3952dc7d
ROHIT-18-05 <rohit364710@gmail.com>
2025-07-29 09:03:11 +0530

    commit

--- fluttermy/lib/stop_watch.dart
+++ fluttermy/lib/stop_watch.dart
@@ -9,55 +9,63 @@ class StopwatchExperiemnt extends StatefulWidget {
 }
 
 class _StopwatchExperiemntState extends State<StopwatchExperiemnt> {
-  int seconds = 0;
+  double seconds = 0;
   late Timer timer;
   bool isTicking = false;
+  int millis = 0;
   String _secondtoText() => seconds <= 1 ? 'Second' : 'Seconds';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Stopwatch Experiment'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Text(
-                '$seconds ${_secondtoText()}',
-                style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: const Text('Stopwatch Experiment')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              '$seconds ${_secondtoText()}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: isTicking ? null : _starttimer,
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.green),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                ),
+                child: const Text("Start"),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: isTicking ? null : _starttimer,
-                  style: const ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.green),
-                    foregroundColor: MaterialStatePropertyAll(Colors.white),
+              const SizedBox(width: 10),
+               ElevatedButton(
+                onPressed: isTicking ? _stoptimer : null,
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                    Color.fromARGB(255, 1, 85, 241),
                   ),
-                  child: const Text("Start"),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
                 ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: isTicking ? _stoptimer : null,
-                  style: const ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.red),
-                    foregroundColor: MaterialStatePropertyAll(Colors.white),
-                  ),
-                  child: const Text("Stop"),
+                child: const Text("Lap"),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: isTicking ? _stoptimer : null,
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.red),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
                 ),
-              ],
-            ),
-          ],
-        )
-      );
+                child: const Text("Stop"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -66,10 +74,10 @@ class _StopwatchExperiemntState extends State<StopwatchExperiemnt> {
   }
 
   void _starttimer() {
-    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+    timer = Timer.periodic(const Duration(milliseconds: 100), _onTick);
     setState(() {
       isTicking = true;
-      seconds = 0;
+      millis = 0;
     });
   }
 
@@ -83,7 +91,8 @@ class _StopwatchExperiemntState extends State<StopwatchExperiemnt> {
   void _onTick(Timer timer) {
     if (mounted) {
       setState(() {
-        seconds++;
+        millis += 100;
+        seconds = millis / 1000;
       });
     }
   }
